06d61569ce16043056085732e287658320037746
Jeremy Stepanovich <jstepanovich91@gmail.com>
2018-04-02 20:42:04 -0500

    backed up original files

---
+++ app/src/components/Discussion/Discussion.js
@@ -0,0 +1,5 @@
+// import React, { Component } from "react";
+// import socketIOClient from "socket.io-client";
+
+
+// export default Discussion;
--- app/src/components/ShowContainer/ShowContainer.js
+++ app/src/components/ShowContainer/ShowContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "../Header";
 import ShowDetail from "../Shows";
+// import Discussion from "../Discussion"
 // import API from "../utils/API";
 
 class ShowContainer extends Component {
@@ -10,6 +11,7 @@ render() {
         <div className="AppClass">
         <Header />
             <ShowDetail />
+            {/* <Discussion /> */}
         </div>
         )
 }
