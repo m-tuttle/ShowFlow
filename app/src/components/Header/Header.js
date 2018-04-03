0fc4de9367a36f661edadafcd619ea5799df6324
Jeremy Stepanovich <jstepanovich91@gmail.com>
2018-04-03 12:08:44 -0500

    backed up initial header files

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,29 +1,15 @@
 import React from "react";
-import { Navbar, Row, Col } from 'react-materialize';
+import { Navbar, NavItem, Icon, Input } from 'react-materialize';
 import "./Header.css";
 
 const Header = props => (
-    <Navbar className="header">
-        <div className="nav-wrapper">
-        <Row>
-          <Col s={4} ><a className="brand-logo black-text left">ShowFlow</a></Col>
-          <Col s={4} ><div className="input-field center">
-          <i className="material-icons prefix">search</i>
-          <input id="icon_prefix" type="text" className="validate black-text"></input>
-          <label for="icon_prefix">Search</label>
-        </div></Col>
-          <Col s={4} >
-            
-          <ul className="right hide-on-med-and-down">
-            <li><a className="black-text" href="#!"><i className="material-icons prefix">home</i></a></li>
-            <li><a className="black-text" href="#!"><i className="material-icons prefix">notifications</i></a></li>
-            <li><a className="black-text" href="#!"><i className="material-icons prefix">person</i></a></li>
-            <li><a className="black-text" href="#!"><i className="material-icons prefix">power_settings_new</i></a></li>
-          </ul>
-          </Col>
-        </Row>
-        </div>
-    </Navbar>
+  <Navbar brand="ShowFlow" className="white" right>
+      <NavItem href="#"><Input className="black-text" label="Search" validate><Icon className="black-text">search</Icon></Input></NavItem>
+      <NavItem href='#'><Icon className="black-text">home</Icon></NavItem>
+      <NavItem href='#'><Icon className="black-text">notifications</Icon></NavItem>
+      <NavItem href='#'><Icon className="black-text">power_settings_new</Icon></NavItem>
+  </Navbar>
 );
 
 export default Header;
+
