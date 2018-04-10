24d9c94434bdf5d985683a287d586f97b1d3289f
Jeremy Stepanovich <jstepanovich91@gmail.com>
2018-04-10 14:27:43 -0500

    appjs update

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ render() {
         <Router>
         <div>
             <Navbar />
+            <Route path="/" component={Login} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/home" component={Home} />
             <Route exact path="/profile" component={Profile} />
