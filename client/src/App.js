947102d3d83633258e383391b0faf42403962571
Jeremy Stepanovich <jstepanovich91@gmail.com>
2018-04-10 14:35:44 -0500

    fixed login paths

--- client/src/App.js
+++ client/src/App.js
@@ -10,23 +10,32 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            loggedIn: true
+            loggedIn: false
         };
     }
     
 render() {
 
-    return (
+    if(!this.state.loggedIn) {
+        return (
+
+            <Login />
+
+        )
+    } 
+    else {
+        return (
+        
         <Router>
         <div>
             <Navbar />
-            <Route path="/" component={Login} />
-            <Route exact path="/login" component={Login} />
+            <Route exact path="/" component={Home} />
             <Route exact path="/home" component={Home} />
             <Route exact path="/profile" component={Profile} />
         </div>
       </Router>
     )
+    }
 }
 
 }
